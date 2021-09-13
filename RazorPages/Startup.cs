347f26f70fd163f17b30698317826caa187c8bf4
Jaxel Rojas <jaxelr@gmail.com>
2021-09-13 16:04:16 -0400

    Including razor pages sample using Rin

---
+++ RazorPages/Startup.cs
@@ -0,0 +1,64 @@
+using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages()
+                .AddRinMvcSupport();
+
+            services.AddDbContext<SchoolContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddRin();
+        }
+        #endregion
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRin();
+
+            app.UseRinMvcSupport();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
